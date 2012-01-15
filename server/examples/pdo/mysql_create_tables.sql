6033798479f233c72b43171c0b199bc8762f3332
Jurriaan Persyn <oemebamo@gmail.com>
2012-01-15 15:41:06 +0100

    Add missing user_id column for table auth_codes to create tables file for PDO server sample implementation

--- server/examples/pdo/mysql_create_tables.sql
+++ server/examples/pdo/mysql_create_tables.sql
@@ -3,6 +3,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 CREATE TABLE `auth_codes` (
   `code` varchar(40) NOT NULL,
   `client_id` varchar(20) NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
   `redirect_uri` varchar(200) NOT NULL,
   `expires` int(11) NOT NULL,
   `scope` varchar(250) DEFAULT NULL,
